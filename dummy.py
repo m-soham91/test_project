1cfb1ce9fbe6b26a315d9e7a805b524618756d78
Sagarmoy Sengupta <sagarmoysengupta@gmail.com>
2018-01-10 00:47:55 +0530

    Final Push

--- dummy.py
+++ dummy.py
@@ -1,24 +1,34 @@
-import datetime
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from tabledef import *
+# import datetime
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# from tabledef import *
 
-engine = create_engine('sqlite:///tutorial.db', echo=True)
+# engine = create_engine('sqlite:///tutorial.db', echo=True)
 
-# create a Session
-Session = sessionmaker(bind=engine)
-session = Session()
+# # create a Session
+# Session = sessionmaker(bind=engine)
+# session = Session()
 
-user = User("admin","password")
-session.add(user)
+# user = User("admin","password")
+# session.add(user)
 
-user = User("python","python")
-session.add(user)
+# user = User("python","python")
+# session.add(user)
 
-user = User("jumpiness","python")
-session.add(user)
+# user = User("jumpiness","python")
+# session.add(user)
 
-# commit the record the database
-session.commit()
+# # commit the record the database
+# session.commit()
 
-session.commit()
+# session.commit()
+from Flask import jsonify
+import MySQLdb
+
+db = MySQLdb.connect("localhost" , "sagar" ,"abcd1234" , "logger")
+cursor = db.cursor();
+sql = """SELECT * FROM users""";
+data  = (cursor.execute(sql);
+
+print data;
+# db.close()
